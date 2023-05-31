6ad4b1d3dd2b7737dd8c6de5d54c7b100d41e75e
chinmay jain <chinmay874@gmail.com>
2023-06-01 00:11:39 +0530

    day 3 work complete, who to follow

---
+++ twitter-clone/src/component/HomePage/RightBar/WhoToFollow/Whotofollow.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import style from './Whotofollow.module.css';
+import item from '../../../../DATA.json'
+
+
+
+function Whotofollow() {
+
+    const[data,setData]=useState([]);
+    const[randomitem,setRandomitem]=useState([]);
+
+
+    const getRandomItem=()=>{
+        const shuffleData = data.sort(()=>0.5-Math.random());
+        console.log(shuffleData);
+        const selecteditem=shuffleData.slice(0,4);
+        console.log(selecteditem);
+        setRandomitem(selecteditem);
+    };
+
+    useEffect(()=>{
+        setData(item);
+        getRandomItem();
+    },[data]);
+
+
+
+
+  return (
+    <>
+    <div className={style.followcontainer}>
+        <h2 style={{margin:"5%"}}>Who to follow</h2>
+
+       {
+            randomitem.map((item)=>(
+                <div className={style.Profile} key={item.id}>
+                    <img className={style.profileimg} src={item.user_image}/>
+                    <h4 >
+                        <p className={style.name}>{item.user_name}</p>
+                        <p style={{margin:'8px 8px'}}>{item.user_country}</p>   
+                    </h4> 
+                </div>
+            ))
+       }
+    </div>
+    
+
+    </>
+    
+  )
+}
+
+export default Whotofollow
