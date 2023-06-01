b033919bf0d6a36c3adb8c0f071fc560653afe9b
sahureshma <ssshureshma17299@gmail.com>
2023-06-01 12:55:06 +0530

    searchBar

--- twitter-clone/src/App.jsx
+++ twitter-clone/src/App.jsx
@@ -3,18 +3,21 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 // import Signup from './component/SignUp/SignUp';
 import SignIn from './component/SignIn/SignIn';
 import HomePage from './component/HomePage/HomePage';
-import SignUp from './component/SignUp/signup'
+import SignUp from './component/SignUp/signup';
+import SearchBar from './component/HomePage/RightBar/SearchBar/SearchBar';
+import Twitterdata from'./DATA.json';
 
 
 function App() {
 
   return (
-     
+   
       <Router >
         <Routes>
           <Route path="/signup" element={<SignUp/>}/>
           <Route  path="/home" element={<HomePage/>}/>
           <Route path="/"  element={<SignIn/>}/>
+          <SearchBar placeholder="search twitter" data={Twitterdata}/>
     
 
 
@@ -22,6 +25,7 @@ function App() {
        
       
       </Router>
+      
     
      
    
